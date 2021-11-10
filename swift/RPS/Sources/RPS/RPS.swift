f5b02068071b5ced8188b266a0fdbff4ab6932e7
Robert Gravina <robert.gravina@gmail.com>
2021-11-10 21:58:43 +0900

    Solution for Swift

--- swift/RPS/Sources/RPS/RPS.swift
+++ swift/RPS/Sources/RPS/RPS.swift
@@ -1,5 +1,23 @@
+enum Throw {
+    case rock, paper, scissors
+}
+
+enum GameResult {
+    case p1wins, p2wins, draw
+}
+
 public struct RPS {
-    func play() -> Bool {
-        return true
+    func play(p1: Throw, p2: Throw) -> GameResult {
+        if (p1 == p2) {
+            return .draw
+        }
+
+        if  (p1 == .rock && p2 == .paper) ||
+            (p1 == .paper && p2 == .scissors) ||
+            (p1 == .scissors && p2 == .rock) {
+            return .p2wins
+        }
+
+        return .p1wins
     }
 }
--- swift/RPS/Tests/RPSTests/RPSTests.swift
+++ swift/RPS/Tests/RPSTests/RPSTests.swift
@@ -2,7 +2,39 @@ import XCTest
 @testable import RPS
 
 final class RPSTests: XCTestCase {
-    func testWiring() {
-        XCTAssertTrue(RPS().play())
+    func testRockVsPaper() {
+        XCTAssertEqual(RPS().play(p1: .rock, p2: .paper), .p2wins)
+    }
+
+    func testPaperVsRock() {
+        XCTAssertEqual(RPS().play(p1: .paper, p2: .rock), .p1wins)
+    }
+
+    func testRockVsScissors() {
+        XCTAssertEqual(RPS().play(p1: .rock, p2: .scissors), .p1wins)
+    }
+
+    func testScissorsVsRock() {
+        XCTAssertEqual(RPS().play(p1: .scissors, p2: .rock), .p2wins)
+    }
+
+    func testScissorsVsPaper() {
+        XCTAssertEqual(RPS().play(p1: .scissors, p2: .paper), .p1wins)
+    }
+
+    func testPaperVsScissors() {
+        XCTAssertEqual(RPS().play(p1: .paper, p2: .scissors), .p2wins)
+    }
+
+    func testRockVsRock() {
+        XCTAssertEqual(RPS().play(p1: .rock, p2: .rock), .draw)
+    }
+
+    func testPaperVsPaper() {
+        XCTAssertEqual(RPS().play(p1: .paper, p2: .paper), .draw)
+    }
+
+    func testScissorsVsScissors() {
+        XCTAssertEqual(RPS().play(p1: .scissors, p2: .scissors), .draw)
     }
 }
